950e4f33f90ff389d464f6d4f30b7ea13fba03ab
Jonathan Horta <jonabc7@gmail.com>
2017-12-01 12:13:52 -0500

    ajuste

--- webhook/views.py
+++ webhook/views.py
@@ -107,9 +107,9 @@ def send_price_updates():
     message_eth3 = "25K => {} MXN {}".format("{:,f}".format(25000 * ((ether_percent_3 / 100) + 1)), percentage_rep(ether_percent_3))
 
     bot.send(SimpleMessage(settings.FB_ADMIN_ID, message_btc))
+    bot.send(SimpleMessage(settings.FB_ADMIN_ID, message_eth))
     bot.send(SimpleMessage(settings.FB_ADMIN_ID, message_eth2))
     bot.send(SimpleMessage(settings.FB_ADMIN_ID, message_eth3))
-    bot.send(SimpleMessage(settings.FB_ADMIN_ID, message_eth))
 
 
 def percentage_change(amount, currency):
